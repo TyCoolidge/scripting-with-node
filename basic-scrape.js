e9d7f21296c1b6d46105b671ec24dbe4872b4424
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-06-02 10:51:46 -0700

    cleaned up code and combined JSON files

--- basic-scrape.js
+++ basic-scrape.js
@@ -2,10 +2,22 @@ const fs = require("fs");
 const { getComponents, getName, getDesc, trim, getInputs } = require("./regex");
 
 const sourceFile = String(fs.readFileSync("html-pages/basic-functions.html"));
+const sourceFileIntermediate = String(
+   fs.readFileSync("html-pages/intermediate-functions.html")
+);
+const sourceFileFunctional = String(
+   fs.readFileSync("html-pages/functional-functions.html")
+);
+const sourceFileAlgorithm = String(
+   fs.readFileSync("html-pages/algorithm-functions.html")
+);
 
 const components = getComponents(sourceFile);
+const components2 = getComponents(sourceFileIntermediate);
+const components3 = getComponents(sourceFileFunctional);
+const components4 = getComponents(sourceFileAlgorithm);
 
-const componentsObjs = components.map((component) => {
+const componentsObjs = components.map((component, index) => {
    return {
       name: getName(component)[0], // getName takes the parameter of component and creates an array of data, the 0 index allows us to only return the name and nothing else
       desc: trim(getDesc(component)[0]),
@@ -13,20 +25,50 @@ const componentsObjs = components.map((component) => {
       type: "basic", // all scraping basic.html
       typeNum: 100, // basic type
       isFavorite: false, // default
+      order: 100 + index, //specifies index as parameter which increments order number by index
+   };
+});
+const componentsIntermediate = components2.map((component, index) => {
+   return {
+      name: getName(component)[0], // getName takes the parameter of component and creates an array of data, the 0 index allows us to only return the name and nothing else
+      desc: trim(getDesc(component)[0]),
+      inputs: getInputs(component).length, // returns length or number of inputs for a function
+      type: "intermediate", // all scraping basic.html
+      typeNum: 200, // basic type
+      isFavorite: false, // default
+      order: 200 + index,
+   };
+});
+const componentsFunctional = components3.map((component, index) => {
+   return {
+      name: getName(component)[0], // getName takes the parameter of component and creates an array of data, the 0 index allows us to only return the name and nothing else
+      desc: trim(getDesc(component)[0]),
+      inputs: getInputs(component).length, // returns length or number of inputs for a function
+      type: "functional", // all scraping basic.html
+      typeNum: 300, // basic type
+      isFavorite: false, // default
+      order: 300 + index,
+   };
+});
+const componentsAlgorithm = components4.map((component, index) => {
+   return {
+      name: getName(component)[0], // getName takes the parameter of component and creates an array of data, the 0 index allows us to only return the name and nothing else
+      desc: trim(getDesc(component)[0]),
+      inputs: getInputs(component).length, // returns length or number of inputs for a function
+      type: "algorithm", // all scraping basic.html
+      typeNum: 400, // basic type
+      isFavorite: false, // default
+      order: 400 + index,
    };
 });
 
-const reversedObjs = componentsObjs.reverse(); //reverses the elements in an array
+var orderObjs = componentsObjs.concat(
+   //.concat can take multiple parametes to combine multiple arrays
+   componentsIntermediate,
+   componentsFunctional,
+   componentsAlgorithm
+);
 
-const orderObjs = [];
-for (let i = 0; i < reversedObjs.length; i++) {
-   const obj = componentsObjs[i]; //finds individual objects or index
-   obj.order = obj.typeNum + i; //takes the typeNum value (100) + whatever index we are on
-   orderObjs.push(obj); //pushed that new object number to main array
-}
-
-console.log(orderObjs.reverse());
-
-const targetFile = "./json-files/basic.json";
+const targetFile = "./dist/dist.json";
 
 fs.writeFileSync(targetFile, JSON.stringify(orderObjs)); //creates a new file containing this data to the above const
